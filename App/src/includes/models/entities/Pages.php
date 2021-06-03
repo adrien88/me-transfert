6ca5422b5b688dd69a5a4afe9f8a634218c6c6a9
= <=>
2021-06-03 11:34:41 +0200

    add pages entities

---
+++ App/src/includes/models/entities/Pages.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    [
+        'filename' => 'home.html',
+        'title' => 'Accueil',
+        'description' => '',
+        'content' => '',
+        'callback' => '',
+    ],
+    [
+        'filename' => 'about.html',
+        'title' => 'A propos',
+        'description' => '',
+        'content' => '',
+        'callback' => '',
+    ],
+];
