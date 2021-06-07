592a9b453abf954f6f912e873bdc810dba684d0e
= <=>
2021-06-07 14:49:43 +0200

    add twig rendering

--- App/src/includes/models/Page.php
+++ App/src/includes/models/Page.php
@@ -10,7 +10,27 @@ class Page
 
     function __construct(?string $slug = 'default.html')
     {
+
+        // trait : page init
         $this->init($slug);
+
+        // page default
+        $this->DOM['head']['lang'] = 'en';
+        $this->DOM['head']['charset'] = 'utf-8';
+        $this->DOM['head']['title'] = 'Me-Transfert';
+
+        $this->DOM['meta']['assets'] = RELPATH . '/App/src/assets';
+        $this->DOM['meta']['routes'] = RELPATH;
+
+        // page navigation
+        $this->DOM['navlinks'] = [];
+        foreach ($this->list() as $file) {
+            $file = substr(basename($file), 0, -5);
+            $text = substr($file, 0, -5);
+            $link = RELPATH . '/page/' . $file;
+            // $a = '<a href="' .  . '">' . substr($file, 0, -5) . ' </a> ';
+            $this->DOM['navlinks'][$text] = $link;
+        }
     }
 
     /**
--- App/src/includes/views/Page.php
+++ App/src/includes/views/Page.php
@@ -3,53 +3,19 @@
 namespace App\views;
 
 use App\models\Page as ModelsPage;
+use App\Router\Route;
 
 class Page
 {
-    function __construct(ModelsPage $page, string $model = 'App/src/assets/html/index.php')
+    function __construct(ModelsPage $page, string $model = 'App/src/assets/html/index.html')
     {
-        /**
-         * prepare page elements here ! 
-         * compose menu & onter stuffs
-         */
-        if (file_exists($model) && is_file($model))
-            include $model;
-    }
+        $loader = new \Twig\Loader\FilesystemLoader('App/src/assets/html');
+        $twig = new \Twig\Environment($loader, [
+            // uncomment to enable cache !
+            // 'cache' => 'App/cache',
+        ]);
 
-    /**
-     * 
-     */
-    function parse()
-    {
-    }
-
-    /**
-     * 
-     */
-    function parseVariables(string &$content, array $data = [])
-    {
-        if (0 != ($nbr = preg_match('/\$[\w]/i', $content, $matches)))
-            foreach ($matches as $match) {
-                $content = str_replace($match, '', $content);
-            }
-        return $nbr;
-    }
-
-    /**
-     * 
-     */
-    function parseFiles(string &$content)
-    {
-        if (0 != ($nbr = preg_match('/\#[\w\/\.\-]/i', $content, $matches)))
-            foreach ($matches as $match) {
-                $file = substr($match, 1);
-                if (file_exists($file)) {
-                    $include = file_get_contents($file);
-                    // garder espace pour parser 
-                    // $this->parseFiles($include);
-                    $content = str_replace($match, $include, $content);
-                }
-            }
-        return $nbr;
+        $template = $twig->load($page->filename);
+        echo $template->render($page->DOM);
     }
 }
