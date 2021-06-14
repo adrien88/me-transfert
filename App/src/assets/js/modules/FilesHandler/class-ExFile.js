940ab44083a5e2dd28e23c84a387ed662972276f
= <=>
2021-06-14 15:20:48 +0200

    Add ExFile class

---
+++ App/src/assets/js/modules/FilesHandler/class-ExFile.js
@@ -0,0 +1,132 @@
+export class ExFile {
+
+    constructor(file = null) {
+        if (null != file){
+            this.import(file);
+        } else {
+            this.file = new File();
+        }
+       
+    }
+
+    /**
+     * Read file as stream.
+     * Alias of File.stream()
+     *
+     * @returns stream
+     */
+    stream() {
+        return this.file.stream();
+    }
+
+    /**
+     * Reaf file as buffer.
+     * Alias of File.arrayBuffer() hineritence of clob Vlasse.
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/Blob/arrayBuffer
+     *
+     * @returns Promise content blob binary data in an arrayBuffer.
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer
+     */
+    async arrayBuffer() {
+        return await this.file.arrayBuffer();
+    }
+
+    /**
+     * Import file (from drag&drop or input form).
+     * 
+     * @Param {File} file to load.
+     * 
+     */
+    import(file){
+        if (file instanceof File){
+            this.file = file;
+            this.name = file.name;
+            this.type = file.type;
+            this.lastMofified = file.lastMofified;
+            this.imported = Date.now();
+        }
+    }
+
+    /**
+     * Import from server.
+     *
+     * @param {string} uri [optional]
+     */
+    load(url = null) {
+
+        target = this.url ?? url;
+        fetch(target, {
+            method: "GET",
+        })
+            .then((response) => {
+                if (response.ok) return response.blob();
+                else {
+                    console.log("File : " + uri);
+                    console.log("Network error : " + response.statusText);
+                }
+            })
+            .then((data) => {
+                file = new File(data, uri);
+                file.uri = uri;
+                // FileList.set(file);
+            })
+            .catch((errors) => {
+                console.log(errors);
+            });
+    }
+
+    /**
+     * Unlink file on server
+     * 
+     */
+    async unlink() {
+        if (null != this.url) {
+            this.url.replace("send", "delete");
+            data = new FormData();
+            data.append("uri", this.uri);
+            data.append("filename", this.filename);
+            data.append("type", this.type);
+            
+            return await fetch(this.url, {
+                method: "POST",
+                body: data,
+            })
+                .then((response) => {
+                    if (response.ok) return response.json();
+                    else console.log("Network error : " + response.statusText);
+                })
+                .catch((errors) => {
+                    console.log(errors);
+                });
+        }
+    }
+
+    /**
+     * Save file on server
+     *
+     * @param {string} string
+     */
+    async save() {
+        if (null != this.url) {
+            data = new FormData;
+            data.append("file.uri", this.uri);
+            data.append("file.filename", this.filename);
+            data.append("file.type", this.type);
+            data.append("file.payload", this.file);
+
+            return await fetch(url, {
+                method: "POST",
+                body: data,
+            })
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                    else console.log("Network error : " + response.statusText);
+                })
+                .catch((errors) => {
+                    console.log(errors);
+                });
+        }
+    }
+}
