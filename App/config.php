5bfd2c9ed9dc4be6547c093b6e11939aa1f47a29
= <=>
2021-06-17 13:49:08 +0200

    add config file

---
+++ App/config.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+return [
+    'PHPMailer' => [
+        'Host' => 'smtp.mailtrap.io',
+        'Port' => '2525',
+        'Auth' => [
+            // 'SMTPSecure' => PHPMailer::ENCRYPTION_STARTTLS,
+            'Username' => '6128c4fb138724',
+            'Password' => 'f18ab039a39017',
+        ],
+    ]
+];
